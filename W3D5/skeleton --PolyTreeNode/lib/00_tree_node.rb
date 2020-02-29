3f69fb88bdd7915dc6d4b25dd80b3595e55e4636
Eric Hsieh <525chungli@gmail.com>
2020-02-28 16:34:17 -0800

    PolyTreeNode

---
+++ W3D5/skeleton --PolyTreeNode/lib/00_tree_node.rb
@@ -0,0 +1,67 @@
+class PolyTreeNode
+require "byebug"
+    attr_accessor :parent, :children, :value
+
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def inspect
+        value.inspect
+    end
+    def parent=(parentnode)   
+        if !@parent.nil?
+            self.parent.children.delete(self)
+        end
+        if parentnode.nil? 
+            @parent = nil
+        else
+            # debugger
+            @parent = parentnode
+            
+            if !parentnode.children.include?(self)
+                parentnode.children << self   
+
+            end
+        end
+        
+    end
+
+    def add_child(childnode) 
+        childnode.parent = self
+    end
+
+    def remove_child(childnode)
+        raise "error" if childnode.parent != self
+
+        childnode.parent = nil
+    end
+
+    def dfs(target) 
+         debugger
+        return self if self.value == target
+
+            self.children.each do |child|
+                search_result = child.dfs(target)
+                return search_result if !search_result.nil?
+            end
+        nil
+    end
+
+    def bfs(target)
+        queue = []
+        queue.push(self)
+        until queue.empty?
+            node = queue.shift
+            return node if node.value == target
+            node.children.each do |child|
+                queue.push(child)
+            end
+        end
+        nil
+    end
+
+
+end
